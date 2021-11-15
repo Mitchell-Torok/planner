ce2151482a31b1b2b1dbbcab3be333063255365a
RyanFurtado <92509462+RyanFurtado@users.noreply.github.com>
2021-11-15 00:00:50 -0800

    Added problem file generation -unfinished

--- src/planner_node.cpp
+++ src/planner_node.cpp
@@ -5,6 +5,7 @@
 #include "plan_parser.hpp"
 //include "planner/planner_node.hpp"
 
+#include <fstream>
 #include <memory>
 #include <string>
 #include <functional>
@@ -31,12 +32,25 @@ public:
 
 
 private:
+	std::string block[10] = {""};
+	int numRooms = 0;
+	 
 	void set_map_info(
 		const std::shared_ptr<comp3431_interfaces::srv::MapInfo::Request> request,
 		const std::shared_ptr<comp3431_interfaces::srv::MapInfo::Response> response)
-	{
-	  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", request->blocks[0].text.c_str());
-	  RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", request->blocks[1].text.c_str());
+	
+	{	
+	  int i = 0;	  
+	  while (i < request->blocks.size()) {
+	  	//RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", request->blocks[i].text.c_str());
+	  	
+	  	block[i] = request->blocks[i].text;
+	  	RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", block[i].c_str());
+	  	i++;
+	  }
+	  
+	  //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %d", request->blocks.size());
+	  //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", request->blocks[1].text.c_str());
 	}
 	
 
@@ -48,10 +62,101 @@ private:
 	  RCLCPP_INFO(this->get_logger(), "Received goal request with order %s", goal->object.c_str());
 	  system("gnome-terminal -- sh -c './src/planner/FF-X/ff -o src/planner/FF-X/tyreworld_domain.pddl -f src/planner/FF-X/tyreworld_facts1 > src/planner/src/plan.txt'");
 	  
-	  
+	  //RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Incoming request %s", block[0].c_str());
 	  //PlanParser plan = new PlanParse("plan.txt");
 	  //RCLCPP_INFO(this->get_logger(), "Received goal request with order %s", PlanParser.getNextStep());
-	  
+		  
+	//Problem Generation 
+	std::string problemPath = "problem.pddl";
+	std::ofstream file;
+	file.open(problemPath);
+	file << "(define (problem moveitemtoroom)" << std::endl;
+	file << " (:domain turtlebot3-domain)" << std::endl;
+	
+	/*
+	//OBJECTS
+	file << " (:objects" << std::endl;
+	file << " turtlebot - robot" << std::endl;
+	//Add Extra Objects e.g. Rooms and Items
+	
+	int i = 0;
+	int m = 0;
+	
+	while (request->blocks[i].text != NULL) {
+		m = 0;
+		char *string = request->blocks[i].text;
+		char *word;
+		word = strtok(string, " ");
+		
+		while(word != NULL) {
+			if (m == 0) {
+				file << word;
+				file << " - room" << std::endl;
+			} else {	
+				file << word;
+				file << " - item" << std::endl;
+			}
+			m++;
+			word = strtok(NULL, " ");	
+		}
+		i++;
+	}
+	
+	file << ")" << std::endl;
+	
+	
+	//INIT
+	file << "(:init" << std::endl;
+	//Add Initial Items to their rooms
+	//ONLY EXAMPLE BELOW
+	i = 0;
+	m = 0;
+	//How to set initial room?
+	while (request->blocks[i].text != NULL) {
+		m = 0;
+		char *string = request->blocks[i].text;
+		char *word = strtok(string, " ");;
+		char *room;
+		while (word != NULL) {
+			if (m == 0) {
+				file << "(in ";
+				room = word;
+			
+			} else {
+				file << "%s ", word;
+			
+			}
+			word = strtok(NULL, " ");	
+			m++;		
+		}
+		file << "%s)", room << std::endl;
+		i++;
+	}
+	file << "(hand_empty turtlebot)" << std::endl;
+	//file << " (at turtlebot initial-room)" << std::endl;
+	file << ")" << std::endl;
+	
+	
+	
+	//GOAL
+	//file << " ";
+	file << "(:goal" << std::endl;
+	//Add Goals from MoveObjectToRoom
+	file << "(in " << std::endl;
+	file << "%s ", goal->object.c_str();
+	file << "%s)", goal->room.c_str() << std::endl;
+	//goal->room.c_str()
+	//goal->object.c_str()
+	
+	file << ")" << std::endl;
+	
+	*/
+	//End of file
+	file << ")" << std::endl;
+	file.close();
+		  
+		  
+	  //Problem Generation ^^
 	  (void)uuid;
 	  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
 	}
